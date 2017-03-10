cd4714f3846f7d56d6fdecb00850e4daff8e6bf9
subinhuang <806519191@qq.com>
2017-03-10 20:32:00 +0800

    install fog

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file  #储存在本地
-  # storage :fog  # 储存在s3
+  # storage :file  #储存在本地
+  storage :fog  # 储存在s3
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
