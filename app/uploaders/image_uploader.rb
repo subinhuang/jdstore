e471d57ab972ad54706962d59a1f95e5b7f17a1a
subinhuang <806519191@qq.com>
2017-03-14 20:48:00 +0800

    modify index page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
 
   version :thumb do
-    process resize_to_fit: [300,200]
+    process resize_to_fit: [200,200]
   end
 
   version :medium do
