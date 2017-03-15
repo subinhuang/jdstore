5bdc965a4bee27caf517b7c83c8173f269af5f75
subinhuang <806519191@qq.com>
2017-03-15 18:33:59 +0800

    add category & modify product index

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -10,15 +10,17 @@ class Admin::ProductsController < ApplicationController
   def new
     @product = Product.new
     @photo = @product.photos.build #for multi-pics
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def edit
     @product = Product.find(params[:id])
+    @categories = Category.all.map{ |c| [c.name, c.id]}
   end
 
   def update
       @product = Product.find(params[:id])
-
+      @product.category_id = params[:category_id]
       if params[:photos] != nil
         @product.photos.destroy_all #need to destroy old pics first
 
@@ -38,10 +40,11 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.category_id = params[:category_id]
     if  @product.save
       if params[:photos] != nil
          params[:photos]['avatar'].each do |a|
-           @photo = @product.photoss.create(:avatar => a)
+           @photo = @product.photos.create(:avatar => a)
          end
       end
       redirect_to admin_products_path
@@ -59,7 +62,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price, :image)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image, :category_id)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@
 class Product < ActiveRecord::Base
    mount_uploader :image, ImageUploader
    has_many :photos
-
    accepts_nested_attributes_for :photos
-
+   belongs_to :category
 end
